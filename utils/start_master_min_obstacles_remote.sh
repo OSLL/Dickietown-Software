1c19fd8598d7da122a9522566115f75976c87aac
Konstantin <pro100kot14@gmail.com>
2017-07-25 16:17:17 +0300

    disable leds

--- utils/start_master_min_obstacles_remote.sh
+++ utils/start_master_min_obstacles_remote.sh
@@ -2,7 +2,7 @@
 roslaunch duckietown_demos master.launch \
 veh:=duckmobile joystick:=false coordination:=true navigation:=true anti_instagram:=false \
 intersectionType:=trafficLight \
-/LED/emitter:=false /LED/detector:=true /LED/interpreter:=true \
+/LED/emitter:=false /LED/detector:=false /LED/interpreter:=false \
 visualization:=false \
 /navigation/apriltags_random:=false apriltags:=false \
 obstacle_avoidance:=true /camera/raw:=true \
