25ad3a3425c8d75def191fe013da0074a5bbc538
Konstantin <pro100kot14@gmail.com>
2017-08-08 16:23:55 +0300

    change some options

---
+++ utils/start_master_test_apriltags.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+roslaunch duckietown_demos master.launch \
+veh:=duckmobile joystick:=false coordination:=true navigation:=true apriltags:=true \
+verbose:=true anti_instagram:=false \
+intersectionType:=plain \
+/LED/emitter:=false /LED/detector:=true /LED/interpreter:=true \
+visualization:=false \
+/camera/raw:=true /camera/raw/rect:=true \
+obstacle_avoidance:=false /obstacle_avoidance/safety:=false /obstacle_avoidance/detection:=false \
+lane_following:=true line_detector_param_file_name:=osll
