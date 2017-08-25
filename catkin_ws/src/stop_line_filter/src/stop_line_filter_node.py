fcbca82cae9a8076c30c6a21c93b729bb8c01eca
Konstantin <pro100kot14@gmail.com>
2017-08-25 16:11:37 +0300

    add log msgs

--- catkin_ws/src/stop_line_filter/src/stop_line_filter_node.py
+++ catkin_ws/src/stop_line_filter/src/stop_line_filter_node.py
@@ -15,7 +15,7 @@ class StopLineFilterNode(object):
         self.lane_pose = LanePose()
 
         ## params
-        self.stop_distance = self.setupParam("~stop_distance", 0.2) # distance from the stop line that we should stop 
+        self.stop_distance = self.setupParam("~stop_distance", 0.2) # distance from the stop line that we should stop
         self.min_segs      = self.setupParam("~min_segs", 2) # minimum number of red segments that we should detect to estimate a stop
         self.off_time      = self.setupParam("~off_time", 2)
         self.lanewidth = 0 # updated continuously below
@@ -65,14 +65,16 @@ class StopLineFilterNode(object):
         good_seg_count=0
         stop_line_x_accumulator=0.0
         stop_line_y_accumulator=0.0
+        rospy.loginfo("processSegments start!")
         for segment in segment_list_msg.segments:
             if segment.color != segment.RED:
                 continue
-            if segment.points[0].x < 0 or segment.points[1].x < 0: # the point is behind us 
+            if segment.points[0].x < 0 or segment.points[1].x < 0: # the point is behind us
                 continue
-
+            rospy.loginfo("segment.points=(%s; %s)", segment.points[0], segment.points[1])
             p1_lane = self.to_lane_frame(segment.points[0])
             p2_lane = self.to_lane_frame(segment.points[1])
+            rospy.loginfo("p1_lane=%s; p2_lane=%s", p1_lane, p2_lane)
             avg_x = 0.5*(p1_lane[0] + p2_lane[0])
             avg_y = 0.5*(p1_lane[1] + p2_lane[1])
             stop_line_x_accumulator += avg_x
@@ -86,20 +88,20 @@ class StopLineFilterNode(object):
             stop_line_reading_msg.at_stop_line = False
             self.pub_stop_line_reading.publish(stop_line_reading_msg)
             return
-        
+
         stop_line_reading_msg.stop_line_detected = True
         stop_line_point = Point()
         stop_line_point.x = stop_line_x_accumulator/good_seg_count
         stop_line_point.y = stop_line_y_accumulator/good_seg_count
         stop_line_reading_msg.stop_line_point = stop_line_point
-        stop_line_reading_msg.at_stop_line = stop_line_point.x < self.stop_distance and math.fabs(stop_line_point.y) < self.lanewidth/4 
-        self.pub_stop_line_reading.publish(stop_line_reading_msg)    
+        stop_line_reading_msg.at_stop_line = stop_line_point.x < self.stop_distance and math.fabs(stop_line_point.y) < self.lanewidth/4
+        self.pub_stop_line_reading.publish(stop_line_reading_msg)
         if stop_line_reading_msg.at_stop_line:
             msg = BoolStamped()
             msg.header.stamp = stop_line_reading_msg.header.stamp
             msg.data = True
             self.pub_at_stop_line.publish(msg)
-   
+
     def to_lane_frame(self, point):
         p_homo = np.array([point.x,point.y,1])
         phi = self.lane_pose.phi
@@ -110,13 +112,12 @@ class StopLineFilterNode(object):
         p_new_homo = T.dot(p_homo)
         p_new = p_new_homo[0:2]
         return p_new
- 
+
     def onShutdown(self):
         rospy.loginfo("[StopLineFilterNode] Shutdown.")
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     rospy.init_node('stop_line_filter',anonymous=False)
     lane_filter_node = StopLineFilterNode()
     rospy.on_shutdown(lane_filter_node.onShutdown)
     rospy.spin()
-
