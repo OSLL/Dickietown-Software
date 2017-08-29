465972c2f7c532059f9b29e3c27a95fbe26a9a0c
Konstantin <pro100kot14@gmail.com>
2017-08-29 13:19:56 +0300

    add debug msg

--- catkin_ws/src/intersection_control/src/open_loop_intersection_control_node.py
+++ catkin_ws/src/intersection_control/src/open_loop_intersection_control_node.py
@@ -148,7 +148,7 @@ class OpenLoopIntersectionNode(object):
 
         published_already = False
         for index, pair in enumerate(self.maneuvers[turn_type]):
-            rospy.loginfo("[%s] drive %s sec", self.node_name, pair[0])
+            rospy.loginfo("[%s] drive %s sec, cmd = %s", self.node_name, pair[0], pair[1])
             cmd = copy.deepcopy(pair[1])
             start_time = rospy.Time.now()
             end_time = start_time + rospy.Duration.from_sec(pair[0])
