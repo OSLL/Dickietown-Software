a3079805d6ffe97ba7fd3a9b1166eb4a8032dce9
Konstantin <pro100kot14@gmail.com>
2017-08-29 13:37:09 +0300

    try hack

--- catkin_ws/src/intersection_control/src/open_loop_intersection_control_node.py
+++ catkin_ws/src/intersection_control/src/open_loop_intersection_control_node.py
@@ -127,7 +127,7 @@ class OpenLoopIntersectionNode(object):
         rospy.loginfo("old exec_time = %s, new_exec_time = %s" ,exec_time, new_exec_time)
         #new_car_cmd = Twist2DStamped(v=car_cmd.v,omega=(car_cmd.omega - self.lane_pose.phi/new_exec_time))
         # minus (-self.stop_line_reading.line_angle) because need to use bot angle in lane instead stop-line angle
-        new_car_cmd = Twist2DStamped(v=car_cmd.v,omega=(car_cmd.omega - (-self.stop_line_reading.line_angle)/new_exec_time))
+        new_car_cmd = Twist2DStamped(v=car_cmd.v,omega=(car_cmd.omega - (-self.stop_line_reading.line_angle * 10)/new_exec_time))
         new_first_leg = [new_exec_time,new_car_cmd]
         rospy.loginfo("old car command: %s (omega=%s)", str(car_cmd), car_cmd.omega )
         rospy.loginfo("new car command: %s (omega=%s)", str(new_car_cmd), new_car_cmd.omega)
@@ -155,7 +155,7 @@ class OpenLoopIntersectionNode(object):
             while rospy.Time.now() < end_time:
                 if not self.mode == "INTERSECTION_CONTROL": # If not in the mode anymore, return
                     return
-                rospy.loginfo("[%s] publishing cmd = %s", self.node_name, pair[1])    
+                rospy.loginfo("[%s] publishing cmd = %s", self.node_name, pair[1])
                 cmd.header.stamp = rospy.Time.now()
                 self.pub_cmd.publish(cmd)
                 if index > 2:
